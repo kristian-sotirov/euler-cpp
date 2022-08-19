817d1d97c3f5ecf2460566dda651a1931b8efee4
kristian-sotirov <kristian.sotirov@gmail.com>
2022-08-19 18:14:23 +0300

    Initialised necessary functions

--- euler.cpp
+++ euler.cpp
@@ -2,6 +2,23 @@
 #include "euler.h"
 
 
-tuple<vector<float>, float, vector<float>> euler_solver(vector<vector<float>> &K, vector<vector<float>> &M, vector<float> &u_0, vector<float> &f) {
+tuple<vector<double>, double, vector<double>> eulerSolver(vector<vector<double>> &K, vector<vector<double>> &M, vector<double> &u_0, vector<double> &f) {
 
 }
+
+double gethmax(vector<vector<double>> KinvM) {
+
+}
+
+double findMaxEigenvalue(KinvM) {
+
+}
+
+vector<double> solveSingleStep(vector<double> &u_k vector<vector<double>> &P) {
+
+}
+
+vector<vector<double>> createP(vector<vector<double>>) {
+
+}
+
