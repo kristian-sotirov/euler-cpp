b13a0f1bdb357abb5c2649559504aac8a1334c18
kristian-sotirov <kristian.sotirov@gmail.com>
2022-08-19 14:27:42 +0300

    Initial Function Created

---
+++ euler.cpp
@@ -0,0 +1,7 @@
+#include <vector>
+#include "euler.h"
+
+
+tuple<vector<float>, float, vector<float>> euler_solver(vector<vector<float>> &K, vector<vector<float>> &M, vector<float> &u_0, vector<float> &f) {
+
+}
