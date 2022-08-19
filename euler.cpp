9193c4c9c57c80d3470c8f1758d09b89a9eb8a04
kristian-sotirov <kristian.sotirov@gmail.com>
2022-08-19 20:22:03 +0300

    Added more functions and wrote matrix multiplication

--- euler.cpp
+++ euler.cpp
@@ -1,8 +1,12 @@
+#include <iostream>
 #include <vector>
 #include "euler.h"
+using namespace std;
 
 
 tuple<vector<double>, double, vector<double>> eulerSolver(vector<vector<double>> &K, vector<vector<double>> &M, vector<double> &u_0, vector<double> &f) {
+
+
 
 }
 
@@ -18,7 +22,32 @@ vector<double> solveSingleStep(vector<double> &u_k vector<vector<double>> &P) {
 
 }
 
-vector<vector<double>> createP(vector<vector<double>>) {
+vector<vector<double>> createP(vector<vector<double>> &M) {
 
 }
 
+vector<vector<double>> multMatrix(vector<vector<double>> &A, vector<vector<double>> &B) {
+
+	int m = A.size();
+	int p = A[0].size();
+	
+	if(p != B.size()) {
+		cout << "The matrices cannot be multiplied";
+		return A;
+	}
+	int n = B[0].size();
+	
+	vector<vector<double>> C(m, vector<double>(n));
+	
+	for(int i = 0; i < m; i++) {
+		for(int j = 0; j < n; j++) {
+		
+			for(int k = 0; k < p; k++) {
+				
+				C[i][j] += А[i][k]*B[k][j];
+			}
+		}
+	}
+	
+	return C;
+}
