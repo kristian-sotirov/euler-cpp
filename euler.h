d8b0c203d04030af8ee731604defc6a23f06b4f4
kristian-sotirov <kristian.sotirov@gmail.com>
2022-09-06 19:18:07 +0300

    Described functions in the header

--- euler.h
+++ euler.h
@@ -1,12 +1,17 @@
 #ifndef EULER_H
 #define EULER_H
 
-tuple<std::vector<int>,int,std::vector<int>> euler_function {
+tuple<std::vector<int>,int,std::vector<int>> eulerSolver(vector<vector<double>> &K, vector<vector<double>> &M, vector<double> &u_0, vector<double> &f);
 
-	const float tol =
+double gethmax(vector<vector<double>> KinvM);
 
+double findMaxEigenvalue(KinvM);
 
-}
+vector<double> solveSingleStep(vector<double> &u_k vector<vector<double>> &P);
+
+vector<vector<double>> createP(vector<vector<double>> &M);
+
+vector<vector<double>> multMatrix(vector<vector<double>> &A, vector<vector<double>> &B);
 
 
 #endif
